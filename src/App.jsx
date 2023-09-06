2838f0c2161618f79b48bf83fe588af7968ab994
Ian Cenry <iancenry@gmail.com>
2023-09-07 00:04:20 +0300

    setup theme/color contexts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css';
+import { Topbar } from './scenes';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <>
-      <div></div>
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
