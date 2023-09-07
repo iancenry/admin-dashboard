c92c5c73bb61cfe430651d7e13d860b017175e3f
Ian Cenry <iancenry@gmail.com>
2023-09-07 23:27:39 +0300

    feat: add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import {
   Topbar,
   Dashboard,
-  Team,
-  Invoices,
-  Contacts,
-  Bar,
-  Form,
-  Line,
-  Pie,
-  FAQ,
-  Geography,
-  Calendar,
+  Sidebar,
+  // Team,
+  // Invoices,
+  // Contacts,
+  // Bar,
+  // Form,
+  // Line,
+  // Pie,
+  // FAQ,
+  // Geography,
+  // Calendar,
 } from './scenes';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
@@ -24,11 +25,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
+              {/* <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
@@ -37,7 +39,7 @@ function App() {
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/calendar" element={<Calendar />} /> */}
             </Routes>
           </main>
         </div>
