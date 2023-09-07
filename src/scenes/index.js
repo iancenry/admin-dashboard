0996bbce8f91b4db7aedb4ca0bfa4de2a10d6654
Ian Cenry <iancenry@gmail.com>
2023-09-07 22:56:45 +0300

    chore: add new scenes/pages

---
+++ src/scenes/index.js
@@ -0,0 +1,29 @@
+import Dashboard from './dashboard/Dashboard';
+import Sidebar from './global/Sidebar';
+import Topbar from './global/Topbar';
+import Team from './team/Team';
+import Invoices from './invoices/Invoices';
+import Contacts from './contacts/Contacts';
+import Bar from './bar/Bar';
+import Form from './form/Form';
+import Line from './line/Line';
+import Pie from './pie/Pie';
+import FAQ from './faq/FAQ';
+import Geography from './geography/Geography';
+import Calendar from './calendar/Calendar';
+
+export {
+  Dashboard,
+  Sidebar,
+  Topbar,
+  Team,
+  Invoices,
+  Contacts,
+  Bar,
+  Form,
+  Line,
+  Pie,
+  FAQ,
+  Geography,
+  Calendar,
+};
