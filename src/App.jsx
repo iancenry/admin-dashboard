90c3e3f89000d94b8bc0cafec972dc53779c5b60
Ian Cenry <iancenry@gmail.com>
2023-09-07 22:56:24 +0300

    chore: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
-import { Topbar } from './scenes';
+import { Route, Routes } from 'react-router-dom';
+import {
+  Topbar,
+  Dashboard,
+  Team,
+  Invoices,
+  Contacts,
+  Bar,
+  Form,
+  Line,
+  Pie,
+  FAQ,
+  Geography,
+  Calendar,
+} from './scenes';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
@@ -12,6 +26,19 @@ function App() {
         <div className="app">
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/geography" element={<Geography />} />
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
