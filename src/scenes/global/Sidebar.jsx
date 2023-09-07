5f708c2e5816ba260f8f0fc682b38f2acdfa403a
Ian Cenry <iancenry@gmail.com>
2023-09-07 23:27:10 +0300

    chore: add imrts

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,3 +1,30 @@
+import { useState } from 'react';
+import {
+  Sidebar as ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+} from 'react-pro-sidebar';
+import './scss/styles.scss';
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { tokens } from '../../theme';
+
+import {
+  HomeOutlined,
+  PeopleOutlined,
+  ContactsOutlined,
+  ReceiptOutlined,
+  PersonOutlined,
+  CalendarTodayOutlined,
+  HelpOutlined,
+  BarChartOutlined,
+  PieChartOutlineOutlined,
+  TimelineOutlined,
+  MenuOutlined,
+  MapOutlined,
+} from '@mui/icons-material';
+
 const Sidebar = () => {
   return <div>Sidebar</div>;
 };
