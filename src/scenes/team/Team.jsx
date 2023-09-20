77c5f903016dc85d4b60324846c30c56b36da097
Ian Cenry <iancenry@gmail.com>
2023-09-21 00:21:48 +0300

    feat: complete team data grid

--- src/scenes/team/Team.jsx
+++ src/scenes/team/Team.jsx
@@ -1,7 +1,106 @@
-import React from 'react';
+import { Typography, Box, useTheme } from '@mui/material';
+import {
+  AdminPanelSettingsOutlined,
+  LockOpenOutlined,
+  SecurityOutlined,
+} from '@mui/icons-material';
+
+import { DataGrid } from '@mui/x-data-grid';
+import { mockDataTeam } from '../../data/mockData';
+
+import { tokens } from '../../theme';
+import { Header } from '../../components';
 
 const Team = () => {
-  return <div>Team</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  //field should match database name(mockData for this) and headerName will be displayed in the app in place of it
+  //flex 1(like CSS), cellClassName for css, renderCell to give a custom look
+  const columns = [
+    { field: 'id', headerName: 'ID' },
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      cellClassName: 'name-column__cell',
+    },
+    { field: 'email', headerName: 'Email', flex: 1 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      headerAlign: 'left',
+      align: 'left',
+    },
+    { field: 'phone', headerName: 'Phone', flex: 1 },
+    {
+      field: 'access',
+      headerName: 'Access Level',
+      flex: 1,
+      headerAlign: 'center',
+      //destructure row and get the access property
+      renderCell: ({ row: { access } }) => {
+        return (
+          <Box
+            width="60%"
+            m="0 auto"
+            p="5px"
+            display="flex"
+            justifyContent="center"
+            backgroundColor={
+              access === 'admin'
+                ? colors.greenAccent[600]
+                : colors.greenAccent[700]
+            }
+            borderRadius="4px"
+          >
+            {access === 'admin' && <AdminPanelSettingsOutlined />}
+            {access === 'manager' && <SecurityOutlined />}
+            {access === 'user' && <LockOpenOutlined />}
+            <Typography color={colors.grey[100]} ml="5px">
+              {access}
+            </Typography>
+          </Box>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header title="TEAM" subtitle="Manage Team Members" />
+      {/* Target datagrid classes from inspect in browser */}
+      <Box
+        m="40px 0 0 0"
+        height="75svh"
+        sx={{
+          '& .MuiDataGrid-root': {
+            border: 'none',
+          },
+          '& .MuiDataGrid-cell': {
+            borderBottom: 'none',
+          },
+          '& .name-column__cell': {
+            color: colors.greenAccent[300],
+          },
+          '& .MuiDataGrid-columnHeaders': {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: 'none',
+          },
+          '& .MuiDataGrid-virtualScroller': {
+            backgroundColor: colors.primary[400],
+          },
+          '& .MuiDataGrid-footerContainer': {
+            borderTop: 'none',
+            backgroundColor: colors.blueAccent[700],
+          },
+        }}
+      >
+        <DataGrid rows={mockDataTeam} columns={columns} />
+      </Box>
+    </Box>
+  );
 };
 
 export default Team;
