0d7d2f70a96ccdc5742afac40b52cb6c5fe456e7
Ian Cenry <iancenry@gmail.com>
2023-09-22 23:01:29 +0300

    chore: add components

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import {
   Topbar,
   Dashboard,
   Sidebar,
-  // Team,
-  // Invoices,
-  // Contacts,
+  Team,
+  Invoices,
+  Contacts,
   // Bar,
-  // Form,
+  Form,
   // Line,
   // Pie,
   // FAQ,
   // Geography,
-  // Calendar,
+  Calendar,
 } from './scenes';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
@@ -30,16 +30,16 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
+              <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
+              <Route path="/calendar" element={<Calendar />} />
+              {/* <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/calendar" element={<Calendar />} /> */}
+              <Route path="/geography" element={<Geography />} />*/}
             </Routes>
           </main>
         </div>
