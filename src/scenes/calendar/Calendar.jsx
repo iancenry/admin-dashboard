b878900c9550a98613ee3870a248fe24dfb0ccca
Ian Cenry <iancenry@gmail.com>
2023-09-24 22:39:39 +0300

    chore: add Calendar page

--- src/scenes/calendar/Calendar.jsx
+++ src/scenes/calendar/Calendar.jsx
@@ -1,5 +1,68 @@
+import { useState } from 'react';
+
+import { formatDate } from '@fullcalendar/core';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme,
+} from '@mui/material';
+
+import { Header } from '../../components';
+import { tokens } from '../../theme';
+
 const Calendar = () => {
-  return <div>Calendar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  const handleDateClick = (selected) => {
+    console.log(selected);
+    //Switch to a modal
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    }
+  };
+
+  const handleEventClick = (selected) => {
+    // replace with modal
+    if (
+      window.confirm(
+        `Are you sure you want to delete the event ${selected.event.title}`
+      )
+    ) {
+      selected.event.remove();
+    }
+  };
+
+  return (
+    <Box m="20px">
+      <Header title="CALENDAR" subtitle="Check calendar events"></Header>
+
+      <Box display='flex' justifyContent='space-between' >
+        {/* CALENDAR SIDEBAR */}
+        <Box flex='1 1 20%'></Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Calendar;
