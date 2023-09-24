71f625e9275e97931f7262d202171679f9594596
Ian Cenry <iancenry@gmail.com>
2023-09-24 23:17:51 +0300

    feat: Calendar

--- src/scenes/calendar/Calendar.jsx
+++ src/scenes/calendar/Calendar.jsx
@@ -57,9 +57,70 @@ const Calendar = () => {
     <Box m="20px">
       <Header title="CALENDAR" subtitle="Check calendar events"></Header>
 
-      <Box display='flex' justifyContent='space-between' >
+      <Box display="flex" justifyContent="space-between">
         {/* CALENDAR SIDEBAR */}
-        <Box flex='1 1 20%'></Box>
+        <Box
+          flex="1 1 20%"
+          backgroundColor={colors.primary[400]}
+          p="15px"
+          borderRadius="4px"
+        >
+          <Typography variant="h5">Events</Typography>
+          <List>
+            {currentEvents.map((event) => (
+              <ListItem
+                key={event.id}
+                sx={{
+                  backgroundColor: colors.greenAccent[500],
+                  m: '10px 0',
+                  borderRadius: '2px',
+                }}
+              >
+                <ListItemText
+                  primary={event.title}
+                  secondary={
+                    <Typography>
+                      {formatDate(event.start, {
+                        year: 'numeric',
+                        month: 'short',
+                        day: 'numeric',
+                      })}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+
+        {/* CALENDAR */}
+        <Box flex="1 1 100%" ml="15px">
+          <FullCalendar
+            height="75vh"
+            plugins={[
+              dayGridPlugin,
+              timeGridPlugin,
+              interactionPlugin,
+              listPlugin,
+            ]}
+            headerToolbar={{
+              left: 'prev,next,today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth',
+            }}
+            initialView="dayGridMonth"
+            editable
+            selectable
+            selectMirror
+            dayMaxEvents
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              { id: 1213, title: 'All-day event', date: '2023-09-25' },
+            ]}
+          />
+        </Box>
       </Box>
     </Box>
   );
