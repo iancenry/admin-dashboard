ede55b149c6a68e204e30a6bfc1fe16148ed9abd
Ian Cenry <iancenry@gmail.com>
2023-09-25 23:33:54 +0300

    chore: minor changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,15 @@
 import Header from './Header';
 import BarChart from './BarChart';
+import PieChart from './PieChart';
+import LineChart from './LineChart';
+import GeographyChart from './GeographyChart';
+import ProgressCircle from './ProgressCircle';
 
-export { Header, BarChart };
+export {
+  Header,
+  BarChart,
+  PieChart,
+  LineChart,
+  GeographyChart,
+  ProgressCircle,
+};
