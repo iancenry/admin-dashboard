9ec863bdac1ad88e20b71c8216e6aa7b09484ec7
Ian Cenry <iancenry@gmail.com>
2023-09-28 22:16:37 +0300

    add geography  chart page

--- src/scenes/geography/Geography.jsx
+++ src/scenes/geography/Geography.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { Box, useTheme } from '@mui/material';
+import { Header, GeographyChart } from '../../components';
+
+import { tokens } from '../../theme';
 
 const Geography = () => {
-  return <div>Geography</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Header title="Geography Chart" subtitle="Simple Geography Chart" />
+      <Box
+        height="75svh"
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius="4px"
+      >
+        <GeographyChart />
+      </Box>
+    </Box>
+  );
 };
 
 export default Geography;
